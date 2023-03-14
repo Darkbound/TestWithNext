2d23db49a8d398a5471ff2ae7b38110f013aae92
Kostadin Madjerski <dinka4aps@gmail.com>
2023-03-14 11:35:53 +0200

    Fixed useless media queryies

--- sections/AgencySection/elements.jsx
+++ sections/AgencySection/elements.jsx
@@ -10,11 +10,11 @@ export const StyledImageContainer = styled.div`
   width:370px;
   height:610px;
   margin-left:240px;
+  margin-bottom:50px;
   @media(max-width:1024px){
     width:450px;
     margin-left:auto;
     margin-right:auto;
-    margin-bottom:50px;
   }
 `;
 
@@ -24,11 +24,6 @@ export const StyledContainer = styled((props) => <SectionContainer {...props} />
   align-items: center;
   text-align:center;
   margin-bottom: 150px;
-  @media(max-width:1024px){
-  width:100%;
-  background-size:100%;
-  background-position:40px 180px;
-  };
 `;
 export const StyledSectionContainer = styled(({ height, ...props }) => <SectionContainer {...props} />)`
   background-image: url(${BackgroundImage?.src});
@@ -36,16 +31,11 @@ export const StyledSectionContainer = styled(({ height, ...props }) => <SectionC
   background-position:left 20px;
   background-size:960px;
   margin-bottom: 150px;
-  width: 85%;
+  width: 100%;
   min-height:600px;
   margin-top:30px;
-  @media(min-width:1024px) and (max-width:1400px){
-    width:115%;
-  }
   @media(max-width:1024px){
-    width:100%;
     display:block;
-    text-align:center;
   };
 `;
 
