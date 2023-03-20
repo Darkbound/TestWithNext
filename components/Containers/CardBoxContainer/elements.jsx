7eabb3bd835ba34022a494cd34077dee3e592604
Kostadin Madjerski <dinka4aps@gmail.com>
2023-03-20 18:27:57 +0200

    Fixed height on the cards

--- components/Containers/CardBoxContainer/elements.jsx
+++ components/Containers/CardBoxContainer/elements.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const StyledCardBox = styled((props) => <div {...props} />)`
   display:inline-block ;
   width:90%;
-  height:160px;
   background-color: #f5f5f5;
   border-radius:10px;
   :nth-child(2){
